26ba20187b65b4da1fecfa3d173c1a4c37c82b29
Tyler Poag <poagtyler@yahoo.com>
2018-01-03 11:15:42 -0600

    1_2_4 Published

--- VSP.Presentation/Properties/AssemblyInfo.cs
+++ VSP.Presentation/Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ISP")]
+[assembly: AssemblyTitle("ISPV2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ISP")]
-[assembly: AssemblyCopyright("Copyright 2015")]
+[assembly: AssemblyProduct("ISPV2")]
+[assembly: AssemblyCopyright("Copyright 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
