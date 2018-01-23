efe522a11e267a9dd691e2768d3b37e18da359b6
Tyler Poag <poagtyler@yahoo.com>
2018-01-23 15:21:40 -0600

    Revert "1_2_4 Published"
    This reverts commit 26ba20187b65b4da1fecfa3d173c1a4c37c82b29.

--- VSP.Presentation/Properties/AssemblyInfo.cs
+++ VSP.Presentation/Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ISPV2")]
+[assembly: AssemblyTitle("ISP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ISPV2")]
-[assembly: AssemblyCopyright("Copyright 2018")]
+[assembly: AssemblyProduct("ISP")]
+[assembly: AssemblyCopyright("Copyright 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
