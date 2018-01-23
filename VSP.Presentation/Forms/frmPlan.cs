72241e41a578e6343dbe581279d2fd8d98eb05a4
Tyler Poag <poagtyler@yahoo.com>
2018-01-23 15:23:49 -0600

    Revert "Got the Plan aduditor to display after doubleclick event is achieved.  //TODO Display the plan auditor form when the doubleclick event occurs"
    This reverts commit c09603df91d429d6f2e5f07cf3fef939f077d7fa.

--- VSP.Presentation/Forms/frmPlan.cs
+++ VSP.Presentation/Forms/frmPlan.cs
@@ -344,6 +344,15 @@ namespace VSP.Presentation.Forms
             LoadDgvAuditors();
         }
 
+        private void dgvAuditors_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dgvAuditors.CurrentRow.Index;
+            Guid planAuditorId = new Guid(dgvAuditors.Rows[index].Cells["AuditorId"].Value.ToString());
+            PlanAuditor planAuditors = new PlanAuditor(planAuditorId);
+            frmPlanAuditor frmPlanRecordKeeper = new frmPlanAuditor(frmMain_Parent, planAuditors);
+          //  frmPlanAuditor.FormClosed += frmPlanRecordKeeper_FormClosed;
+        }
+
         private void LoadDgvAdvisors()
         {
             DataTable dataTable = VSP.Business.Entities.PlanAdvisor.GetAssociated(CurrentPlan.PlanId);
@@ -486,7 +495,7 @@ namespace VSP.Presentation.Forms
             }
         }
 
-     /* private void frmPlanAuditor_FormClosed(object sender, FormClosedEventArgs e)
+       /* private void frmPlanAuditor_FormClosed(object sender, FormClosedEventArgs e)
         {
             LoadDgvAuditors();
         }
@@ -1051,15 +1060,6 @@ namespace VSP.Presentation.Forms
                 obj.DeleteRecordFromDatabase();
                 LoadDgvEligibleParticipants();
             }
-        }
-
-        private void dgvAuditors_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int index = dgvAuditors.CurrentRow.Index;
-            Guid planAuditorId = new Guid(dgvAuditors.Rows[index].Cells["AuditorId"].Value.ToString());
-            PlanAuditor planAuditors = new PlanAuditor(planAuditorId);
-            frmPlanAuditor frmPlanAuditor = new frmPlanAuditor(frmMain_Parent, planAuditors);
-            //  frmPlanAuditor.FormClosed += frmPlanRecordKeeper_FormClosed;
         }
 	}
 }
